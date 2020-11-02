2e8b5715d66878d8ad52d3c0a584fd62b0280df9
zynkn <wls5563@gmail.com>
2020-11-03 00:03:13 +0900

    init

--- src/contexts/index.ts
+++
@@ -1,3 +0,0 @@
-export {
-  ColorContext
-} from "./useColorContext";
--- src/contexts/useColorContext.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-
-export const ColorContext = React.createContext<any>("#000");
-
-//export default ColorContext;
